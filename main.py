52dd788f2fa39dd3113429f5a0feb3ce04727ed8
Gilson <gilsoncruz10@gmail.com>
2024-12-22 18:19:52 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,21 @@
+print("jornada de dados")
+
+# Variáveis
+idade = 34
+nome = "Gilson"
+
+# Print
+print(idade)
+
+# Soma
+nova_idade = idade + 1
+print(nova_idade)
+
+# Lista
+lista = [1,2,3,4,5]
+
+# Métodos
+lista_invertida = lista.reverse()
+print(lista_invertida)
+
+print(lista)
